33551ade8b47d393f0e01a011f95f9f69c5614e6
kjxbyz <kjxbyz@163.com>
2024-03-24 15:23:38 +0800

    fix: update site config

--- apps/www/config/site.ts
+++ apps/www/config/site.ts
@@ -1,11 +1,10 @@
 export const siteConfig = {
-  name: "muse-ui",
+  name: "Muse UI",
   url: "https://kjxbyz.com/muse-ui",
   ogImage: "https://kjxbyz.com/muse-ui/og.jpg",
   description:
     "Beautifully designed components that you can copy and paste into your apps.",
   links: {
-    twitter: "https://twitter.com/shadcn",
     github: "https://github.com/muse-ui/muse-ui",
   },
 }

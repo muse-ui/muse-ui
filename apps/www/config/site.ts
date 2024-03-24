14b40e22586f1b3179e0305c098e599cca1bff98
kjxbyz <kjxbyz@163.com>
2024-03-24 17:33:28 +0800

    fix: update site.ts

--- apps/www/config/site.ts
+++ apps/www/config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: "Muse UI",
-  url: "https://kjxbyz.com/muse-ui",
+  url: "https://www.kjxbyz.com/muse-ui",
   ogImage: "https://kjxbyz.com/muse-ui/og.jpg",
   description:
     "Beautifully designed components that you can copy and paste into your apps.",
